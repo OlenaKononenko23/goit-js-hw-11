1902a7e4c4fee896e813f1780d9261461234ab1a
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-03-24 22:38:19 +0000

    Deploying to gh-pages from @ OlenaKononenko23/goit-js-hw-11@b95eb954c45e5bf8798fd693acc8bd39ac239d36 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,18 @@
+import{i as o,S as u}from"./assets/vendor-7659544d.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&i(l)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function d(a){const s="43045088-79308e5151ececf264bff6e88",r="https://pixabay.com/api/",i=new URLSearchParams({key:s,q:a,image_type:"photo",orientation:"horizontal",per_page:100,safesearch:!0}),e=`${r}?${i}`;return fetch(e).then(t=>t.json())}function g(a){const{largeImageURL:s,webformatURL:r,tags:i,likes:e,views:t,comments:l,downloads:m}=a;return`<li class="gallery-item">
+      <a class="gallery-link" href="${s}">
+          <img
+              src="${r}"
+              alt="${i}"
+              width="360"
+              height="200"
+              class="gallery-img"
+          />
+          <ul class="gallery-descript">
+          <li class="gallery-descript__item"><span class="gallery-descript__span">likes</span> ${e}</li>
+          <li class="gallery-descript__item"><span class="gallery-descript__span">Views</span> ${t}</li>
+          <li class="gallery-descript__item"><span class="gallery-descript__span">Coments</span> ${l}</li>
+          <li class="gallery-descript__item"><span class="gallery-descript__span">Dowloads</span> ${m}</li>
+       </ul>
+      </a>
+  </li>`}function f(a){return a.map(g).join("")}const n=document.querySelector(".form"),c=document.querySelector(".gallery"),p=document.querySelector(".loader");n.addEventListener("submit",a=>{a.preventDefault(),c.innerHTML="";const s=a.target.elements.value.value;s?(p.classList.remove("is-hidden"),d(s).then(r=>{r.hits.length||o.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"});const i=f(r.hits);c.insertAdjacentHTML("afterbegin",i),p.classList.add("is-hidden")}).then(()=>{new u(".gallery a",{captionsData:"alt",captionDelay:250})}).catch(r=>console.log(r))):o.error({title:"Error",message:"The search field is empty. Please try again!"}),n.reset()});
+//# sourceMappingURL=commonHelpers.js.map
